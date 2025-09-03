9bb8f929d5d38883d9df88a177e47724a2fa4999
siqah <faruoqmuhammed@zetech.ac.ke>
2025-09-03 14:05:33 +0300

    feat: add AvailableRides and Bookings components for ride management and booking functionality

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,8 @@ import DriverDashboard from "./components/driver/DriverDashboard";
 import UpgradeToDriver  from "./components/driver/upgradeToDriver";
 import ProtectedRoute from "./components/guard/ProtectedRoute";
 import CreateRideForm from "./components/driver/CreateRideForm";
+import AvailableRides from "./components/ride/AvailableRides";
+import Bookings from "./components/ride/Bookings";
 import { useAuth } from "./stores/authStore";
 
 const AppRoutes = () => {
@@ -30,6 +32,8 @@ const AppRoutes = () => {
         <Route path="upgrade" element={<UpgradeToDriver />}></Route>
         <Route path="driver-dashboard" element={<DriverDashboard />}></Route>
         <Route path="create-ride" element={<CreateRideForm />}></Route>
+        <Route path="rides" element={<AvailableRides />}></Route>
+        <Route path="bookings" element={<Bookings />}></Route>
         <Route
           path="dashboard"
           element={
